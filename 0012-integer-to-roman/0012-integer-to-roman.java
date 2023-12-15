5c0689147a7c62e37c336ac80947a7cd88a71049
Pranjal Mewara <98090134+simoneetc@users.noreply.github.com>
2023-12-15 23:06:33 +0530

    Time: 3 ms (97.13%), Space: 43.4 MB (60.73%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,9 +1,9 @@
 class Solution {
     public String intToRoman(int num) {
        //like key value pair
-        int[] values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        int[] values=new int[]{1000,900,500,400,100,90,50,40,10,9,5,4,1};
         
-        String[] notations={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        String[] notations=new String[]     {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         StringBuilder sb=new StringBuilder();
 
         for(int i=0;i<values.length;i++){ //iterate values array
